bc791afb17e093d75b5a18310d1f043de1fa3eaf
Tal Vin <taltal3032@gmail.com>
2023-08-13 13:09:11 +0300

    Revert "(Feat) Basic info after redirecting to another page,"
    This reverts commit dce5c95d132f133760b7c22b6da0b6d001eb31d5.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React, { useState } from "react";
-import LocationPage from "./LocationPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homepage from "./components/HomePage";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export const TempType = React.createContext("C");
-
-const App = () => {
-  const [temp, setTemp] = useState("C");
+function App() {
   return (
-    <TempType.Provider value={temp}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />}></Route>
-          <Route path="/location/:placeName" element={<LocationPage />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </TempType.Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
